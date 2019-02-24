9ff9d6d9636c7ca40be33c78eaced9c3f8888784
Alessandro Alves <alves.s.alessandro@gmail.com>
2019-02-24 16:01:33 -0300

    commit linha de base curso spring

---
+++ src/main/java/br/com/casa/cursospring/CursoSpringAplication.java
@@ -0,0 +1,13 @@
+package br.com.casa.cursospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringAplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringAplication.class, args);
+	}
+
+}
