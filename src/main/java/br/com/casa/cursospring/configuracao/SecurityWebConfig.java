2f0f89d8e6fb101ea5621a56563762765e06d955
Alessandro Alves <alves.s.alessandro@gmail.com>
2019-02-25 23:39:44 -0300

    adicionando seguranca

---
+++ src/main/java/br/com/casa/cursospring/configuracao/SecurityWebConfig.java
@@ -0,0 +1,34 @@
+package br.com.casa.cursospring.configuracao;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityWebConfig extends WebSecurityConfigurerAdapter {
+
+	private final static String[] PUBLIC_MATCHERS = { "/curso/api/**" };
+
+	@Override
+	protected void configure(HttpSecurity security) throws Exception {
+		security.cors().and().csrf().disable();
+		
+		security.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
+		
+		security.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+}
